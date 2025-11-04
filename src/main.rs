28a65440269707f57311041c6f346cd196feead4
JamesVictor-O <victorjames40v@gmail.com>
2025-11-04 02:03:24 +0100

    associate functions in rust

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,8 @@
   #[derive(Debug)]
  struct Owner {name:String}
 
-
- struct Car {
+ #[derive(Debug)]
+struct Car {
     owner: String,
     year:u32,
     fuel_level:f32,
@@ -29,14 +29,45 @@
 
 
 
+ // three forms of self a method could take
+    // - First form: An immutable reference to self(&self);
+    // - Second form : A mutable reference to self(&mut self)
+    // - Third form: An owned form of self(self )
 
 
 impl  Car {
+    // - first form: An immutable reference to self(&self);
     fn display_car(&self){
         println!(
             "Owner: {}, Year: {}, Price: {}", self.owner, self.year, self.price
         )
-    }    
+    }   
+
+    // - Second form : A mutable reference to self(&mut self)
+    
+    fn refule(&mut self, gallons:f32){
+        self.fuel_level + gallons;
+    }
+
+
+    // - Third form: An owned form of self(self )
+
+    fn  sell(self) -> Car{
+        self
+    }
+
+    //  Associated functions 
+    //  associate functions are simply functions define inside an impl block, they are accoiated with the type not instances
+    // associate functions do not take self because it's creating a new instance , not acting on exsisting onr
+    
+    fn new(  owner: String,
+    year:u32,
+    fuel_level:f32,
+    price : u32) -> Car{
+        Car {
+            owner, year, fuel_level, price
+        }
+    }
 }
 
 fn main() {
@@ -56,27 +87,29 @@ fn main() {
     println!("{:?}", secondOwner); 
 
     //  
-    let my_car= Car{ owner: String::from("James"), year: 2023, fuel_level:0.0, price: 10_000 };
+    let mut my_car= Car{ owner: String::from("James"), year: 2023, fuel_level:0.0, price: 10_000 };
 
-     my_car.display_car();
+    //  my_car.display_car()
 
-   
-    let car_owner=my_car.owner;
+    my_car.refule(3.0);
 
-    let another_car= Car{
-        owner:String::from("victor"),
-        ..my_car
-    };
-
-    println!("car owner is  {}", car_owner);
+    println!("car fuel level is {}", my_car.fuel_level);
     
     // Tuple struct
     let points_2D: (i32, i32) = (1,3);
     let points2_3D= (4,10, 13);
 
     //  functionality to structs
+    println!("call my car, {:?}", my_car.display_car());
 
+    //  Two requirements for a function to be considered as a method.
+    //  - Must be inside an implementation block
+    //  - first parameter must be self
 
+   let latest_car= Car::new(String::from("ochula"), 2000, 3.0, 8_000);
+
+   println!("the lates model, {:?}", latest_car)
+   
 
 
 
